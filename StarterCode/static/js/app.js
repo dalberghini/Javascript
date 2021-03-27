555e2060565e4b31baa813f2b96b786d89779d90
dalberghini <david.alberghini@gmail.com>
2021-03-27 16:58:47 -0600

    filtering completed.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,12 +1,40 @@
 // from data.js
 var tableData = data;
 var tbody = d3.select("tbody");
+var button = d3.select("button");
+var form = d3.select("form");
 // YOUR CODE HERE!
-tableData.forEach((data_list) =>{
-    console.log(data_list);
-    var row = tbody.append("tr");
-    Object.entries(data_list).forEach(([key,value])=>{
-        var cell = row.append("td");
-        cell.text(value);
+function create_table(tableData) {
+    tableData.forEach((data_list) =>{
+        var row = tbody.append("tr");
+        Object.entries(data_list).forEach(([key,value])=>{
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-});
+}
+create_table(tableData);
+button.on("click", runFilter);
+form.on("submit", runFilter);
+
+
+function runFilter() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    var filteredData = tableData.filter(filtering => filtering.datetime === inputValue);
+    tbody.html("");
+    filteredData.forEach((filtering) => {
+        var row = tbody.append("tr");
+        Object.entries(filtering).forEach(([key,value])=>{
+            var cell = row.append("td");
+            cell.text(value);
+            console.log(value);
+        });
+    });
+
+    if (inputValue === "") {
+        create_table(tableData);
+    }
+}
+
