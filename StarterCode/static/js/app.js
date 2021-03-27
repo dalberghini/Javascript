2eb572d8f00559d37a8a24df528ab1d68df82cb7
dalberghini <david.alberghini@gmail.com>
2021-03-27 16:03:25 -0600

    table view achieved.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,12 @@
 // from data.js
 var tableData = data;
-
+var tbody = d3.select("tbody");
 // YOUR CODE HERE!
+tableData.forEach((data_list) =>{
+    console.log(data_list);
+    var row = tbody.append("tr");
+    Object.entries(data_list).forEach(([key,value])=>{
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
